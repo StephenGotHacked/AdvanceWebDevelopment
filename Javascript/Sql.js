556bf9569a8a2dc7f75566ebe961fdb08e0d0eea
Stephen Condino <101857794+StephenGotHacked@users.noreply.github.com>
2024-04-27 23:54:04 +0800

    Add files via upload

--- Javascript/Sql.js
+++ Javascript/Sql.js
@@ -11,3 +11,73 @@ function Table(){
     document.execCommand('copy');
     alert('Create Table query is copied to clipboard');
 }
+
+function Insert(){
+    var copy = document.getElementById("sql3textarea")
+    copy.select();
+    document.execCommand('copy');
+    alert('Insert query is copied to clipboard');
+}
+
+function Select1(){
+    var copy = document.getElementById("sql4textarea")
+    copy.select();
+    document.execCommand('copy');
+    alert('Select query is copied to clipboard');
+}
+
+function Select2(){
+    var copy = document.getElementById("sql5textarea")
+    copy.select();
+    document.execCommand('copy');
+    alert('Select query is copied to clipboard');
+}
+
+function Select3(){
+    var copy = document.getElementById("sql6textarea")
+    copy.select();
+    document.execCommand('copy');
+    alert('Select query is copied to clipboard');
+}
+
+function Select4(){
+    var copy = document.getElementById("sql7textarea")
+    copy.select();
+    document.execCommand('copy');
+    alert('Select query is copied to clipboard');
+}
+
+function Select5(){
+    var copy = document.getElementById("sql8textarea")
+    copy.select();
+    document.execCommand('copy');
+    alert('Select query is copied to clipboard');
+}
+
+function Update(){
+    var copy = document.getElementById("sql9textarea")
+    copy.select();
+    document.execCommand('copy');
+    alert('Update query is copied to clipboard');
+}
+
+function Delete(){
+    var copy = document.getElementById("sql10textarea")
+    copy.select();
+    document.execCommand('copy');
+    alert('Update query is copied to clipboard');
+}
+
+function Delete2(){
+    var copy = document.getElementById("sql11textarea")
+    copy.select();
+    document.execCommand('copy');
+    alert('Update query is copied to clipboard');
+}
+
+function Delete3(){
+    var copy = document.getElementById("sql12textarea")
+    copy.select();
+    document.execCommand('copy');
+    alert('Update query is copied to clipboard');
+}
